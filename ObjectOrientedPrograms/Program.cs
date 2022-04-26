da1400688bc591670fb0982e3edcd6ec798e8c64
59114_Chetana <deslechetna1999@gmail.com>
2022-04-26 18:10:46 +0530

    [Chetana]Added: Inventory Data Management

--- ObjectOrientedPrograms/Program.cs
+++ ObjectOrientedPrograms/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using ObjectOrientedPrograms.InventoryDataManagement;
+using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        const string INVENTORY_DATA_PATH = @"R:\Practice_problem_Bridgelabz\ObjectOrientedPrograms\ObjectOrientedPrograms\InventoryDataManagement\Inventory.json";
+        InventoryManagement inventoryManagement = new InventoryManagement();
+        bool end = true;
+        Console.WriteLine("1.Display\n 2. End the Program");
+        while (end == true)
+        {
+            Console.WriteLine("Take an option to execute");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    inventoryManagement.DisplayInventory(INVENTORY_DATA_PATH);
+                    break;
+                case 2:
+                    end = false;
+                    break;
+                default:
+                    Console.WriteLine("Enter Proper Option To Execute");
+                    break;
+            }
+        }
+    }
+}
+
